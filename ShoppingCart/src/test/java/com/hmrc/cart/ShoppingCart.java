7976cd212f6bb1145bfb6f70b7d1fef66f78c2f5
ijfessey <ijfessey@hotmail.com>
2016-09-10 14:37:09 +0100

    Set up junit env

---
+++ ShoppingCart/src/test/java/com/hmrc/cart/ShoppingCart.java
@@ -0,0 +1,14 @@
+package com.hmrc.cart;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShoppingCart {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
