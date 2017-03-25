90c8d936c55ef2761d7b510ceaf770472eec0c88
ijfessey <ijfessey@hotmail.com>
2017-03-25 18:43:13 +0000

    length

--- ShoppingCart/src/main/java/com/hmrc/cart/Bannana.java
+++ ShoppingCart/src/main/java/com/hmrc/cart/Bannana.java
@@ -5,7 +5,14 @@ public class Bannana implements CartItem {
 	private String description;
 	private int priceInPence;
 	private String colour;
+	private int length;
 
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
 	public String getDescription() {
 		return description;
 	}
